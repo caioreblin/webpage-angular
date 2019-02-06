c2282f5a13c2418a412de7e41a7c0d7588325778
Placido Antonio Langer Reinert <placidoar@weg.net>
2019-02-06 13:29:13 -0200

    safe commit

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,15 +1,30 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { IProduct } from "./product";
+import { ProductService } from "./product.service";
 
 @Component({
     selector: 'pm-products',
-    templateUrl: './product-list.component.html'
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css'] 
 })
 
-export class ProductListComponent {
+export class ProductListComponent implements OnInit{
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
-    products: any[] = [
+    showImage: boolean = false;
+    
+    _listFilter: string;
+    get listFilter(): string{
+      return this._listFilter;
+    }
+    set listFilter(value: string){
+      this._listFilter = value;
+      this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+    }
+
+    filteredProducts: IProduct [];
+    products: IProduct[] = [
       {
         "productId": 1,
         "productName": "Leaf Rake",
@@ -61,4 +76,27 @@ export class ProductListComponent {
         "imageUrl": "https://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png"
       }
     ];
+
+    constructor(private productService : ProductService){
+
+    }
+
+    onRatingClicked(message: string): void{
+      this.pageTitle = 'Product List: ' + message;
+    }
+
+    performFilter(filterBy: string): IProduct[] {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product: IProduct) =>
+              product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+    }
+
+    toggleImage(): void {
+      this.showImage = !this.showImage;
+    }
+
+    ngOnInit(): void{
+      this.products = this.productService.getProducts();
+      this.filteredProducts = this.products;
+    }
 }
