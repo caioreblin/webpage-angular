3df5dd173c4678857f287c44dbf934824371c955
Placido Antonio Langer Reinert <placidoar@weg.net>
2019-02-07 10:17:21 -0200

    finalized app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,17 @@ import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
-import { ProductListComponent } from './products/product-list.component';
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-import { StarComponent } from './shared/star.component';
 import { HttpClientModule } from '@angular/common/http';
-import { ProductDetailComponent } from './products/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
+import { ProductModule } from './products/product.module';
 
 @NgModule({
   declarations: [
-    AppComponent, ProductListComponent, ConvertToSpacesPipe, StarComponent, ProductDetailComponent, WelcomeComponent
+    AppComponent, WelcomeComponent
   ],
   imports: [
-    BrowserModule, FormsModule, HttpClientModule, RouterModule.forRoot([
-      { path: 'products', component: ProductListComponent },
-      { path: 'products/:id', component: ProductDetailComponent },
+    BrowserModule, HttpClientModule, ProductModule, 
+    RouterModule.forRoot([
       { path: 'welcome', component: WelcomeComponent },
       { path: '', redirectTo: 'welcome', pathMatch: 'full' },
       { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 
 import { IProduct } from "./product";
 
@@ -13,7 +13,7 @@ export class ProductDetailComponent implements OnInit{
     pageTitle: string = 'Product Detail';
     product: IProduct;
 
-    constructor(private route: ActivatedRoute){};
+    constructor(private route: ActivatedRoute, private router: Router){};
 
     ngOnInit(){
         let id = +this.route.snapshot.paramMap.get('id');
@@ -28,5 +28,9 @@ export class ProductDetailComponent implements OnInit{
                 "starRating": 3.2,
                 "imageUrl": "https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
         }
+    }
+
+    onBack(): void{
+        this.router.navigate(['/products'])
     }
 }
