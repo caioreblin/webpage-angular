2a10b204c147246479b81b2bf0c7c653f8157887
Caio Henrique Reblin <caiohr@weg.net>
2019-02-07 14:53:28 -0200

    additional functions

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
 
 import { IProduct } from "./product";
+import { ProductService } from "./product.service";
 
 @Component({
     selector: 'pm-product-detail',
@@ -11,26 +12,30 @@ import { IProduct } from "./product";
 
 export class ProductDetailComponent implements OnInit{
     pageTitle: string = 'Product Detail';
-    product: IProduct;
+    errorMessage = '';
+    product: IProduct | undefined;
 
-    constructor(private route: ActivatedRoute, private router: Router){};
-
-    ngOnInit(){
-        let id = +this.route.snapshot.paramMap.get('id');
-        this.pageTitle += ` ${id}`;
-        this.product = {
-                "productId": 1,
-                "productName": "Leaf Rake",
-                "productCode": "GDN-0011",
-                "releaseDate": "March 19, 2016",
-                "description": "Leaf rake with 48-inch wooden handle.",
-                "price": 19.95,
-                "starRating": 3.2,
-                "imageUrl": "https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+    constructor(private route: ActivatedRoute,
+        private router: Router,
+        private productService: ProductService) {
+      }
+    
+      ngOnInit() {
+        const param = this.route.snapshot.paramMap.get('id');
+        if (param) {
+          const id = +param;
+          this.getProduct(id);
         }
+      }
+    
+      getProduct(id: number) {
+        this.productService.getProduct(id).subscribe(
+          product => this.product = product,
+          error => this.errorMessage = <any>error);
+      }
+    
+      onBack(): void {
+        this.router.navigate(['/products']);
+      }
+    
     }
-
-    onBack(): void{
-        this.router.navigate(['/products'])
-    }
-}
